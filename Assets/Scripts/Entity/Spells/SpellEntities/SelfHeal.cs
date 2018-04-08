c5301bb3d1646f1e7940701c77a5dffcb185a2de
Arthur MASSON <amasson@e2r7p12.42.fr>
2018-04-08 22:22:42 +0200

    heal

---
+++ Assets/Scripts/Entity/Spells/SpellEntities/SelfHeal.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfHeal : SpellEntity {
+
+	// Use this for initialization
+	void Start () {
+		if (caster.unit)
+			caster.unit.TakeDamages(-50.0f * caster.unit.currentSpellDamages, caster.unit);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Destroy(this, 1.0f);
+	}
+
+	public override bool CanBeCastedBy(SpellCaster caster, out string error) {
+		if (caster.caster.unit && caster.caster.unit.currentHealth >= caster.caster.unit.maxHealth) {
+			error = "life is already full";
+			return false;
+		}
+		error = "";
+		return true;
+	}
+}
