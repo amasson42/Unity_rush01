58082945e8ec9f58edfc47517a3282e6e8a74900
qhonore <qhonore@student.42.fr>
2018-04-08 15:59:50 +0200

    move var Loot -> ItemInventory

--- Assets/Scripts/Inventory/Loot.cs
+++ Assets/Scripts/Inventory/Loot.cs
@@ -3,12 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Loot : MonoBehaviour {
-	public enum Rarity {Common, Uncommon, Magic, Epic, Legendary};
-	public enum Type {Weapon, Potion};
-	public static readonly Color[] RarityColors = {Color.white, new Color(0.333f,0.807f,0.196f,1), new Color(0.243f,0.478f,0.909f,1), new Color(0.549f,0.231f,0.76f,1), new Color(0.972f,0.694f,0.168f,1)};
-	public static readonly string[] RarityStrings = {"Common", "Uncommon", "Magic", "Epic", "Legendary"};
-	public static readonly float[] RarityStatsCoef = {1f, 1.2f, 1.4f, 1.8f, 2.5f};
-
 	public float	lootChance = 20f;
 	public ItemEntity[]	loots;
 
@@ -36,14 +30,14 @@ public class Loot : MonoBehaviour {
 		}
 	}
 
-	Rarity getItemRarity()
+	ItemInventory.Rarity getItemRarity()
 	{
 		float rand = Random.Range(0f, 100f);
-		if (rand > 99f) return (Rarity.Legendary);
-		if (rand > 95f) return (Rarity.Epic);
-		if (rand > 90f) return (Rarity.Magic);
-		if (rand > 70f) return (Rarity.Uncommon);
-		return (Rarity.Common);
+		if (rand > 99f) return (ItemInventory.Rarity.Legendary);
+		if (rand > 95f) return (ItemInventory.Rarity.Epic);
+		if (rand > 90f) return (ItemInventory.Rarity.Magic);
+		if (rand > 70f) return (ItemInventory.Rarity.Uncommon);
+		return (ItemInventory.Rarity.Common);
 	}
 
 	bool winLotery(float lootChance)
@@ -56,9 +50,9 @@ public class Loot : MonoBehaviour {
 	void generateStats(ref ItemInventory instance, int level)
 	{
 		instance.rarity = getItemRarity();
-		instance.weaponName = RarityStrings[(int)instance.rarity] + " " + instance.weaponName;
+		instance.weaponName = ItemInventory.RarityStrings[(int)instance.rarity] + " " + instance.weaponName;
 		instance.level = level;
-		instance.minDamage = Random.Range(20f, 25f) * Mathf.Pow(1.1f, level - 1) * RarityStatsCoef[(int)instance.rarity] * instance.attackSpeed;
+		instance.minDamage = Random.Range(20f, 25f) * Mathf.Pow(1.1f, level - 1) * ItemInventory.RarityStatsCoef[(int)instance.rarity] * instance.attackSpeed;
 		instance.maxDamage = Random.Range(instance.minDamage, instance.minDamage * 1.5f);
 		instance.attackSpeed = Random.Range(instance.attackSpeed * 0.75f, instance.attackSpeed * 1.25f);
 	}
