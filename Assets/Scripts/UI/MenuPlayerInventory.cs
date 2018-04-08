cc1168fbe45f4fb43e4c15d3f8792545ac4d3c40
Jessy ROUTHIER <jrouthie@e2r6p10.42.fr>
2018-04-08 21:29:41 +0200

    merge

--- Assets/Scripts/UI/MenuPlayerInventory.cs
+++ Assets/Scripts/UI/MenuPlayerInventory.cs
@@ -7,17 +7,34 @@ using UnityEngine.UI;
 public class MenuPlayerInventory : MonoBehaviour {
 
 
+	public static MenuPlayerInventory instance;
+
 	public Actor player;
 
 	private MenuInterface menu;
 	private Text description;
+	// public static MenuInventoryCase[] list;
+	public List<MenuInventoryCase> list = new List<MenuInventoryCase>();
+	public MenuInventoryCase equip;
+
+	void Awake() {
+		instance = this;
+	}
 
 	void Start ()
 	{
 		menu = GetComponent<MenuInterface>();
 		description = transform.Find("Menu/TabDescription/Text").GetComponent<Text>();
+		for (int i = 0; i < list.Count; ++i)
+			list[i].caseId = i++;
 	}
 	
+	void UpdateInv()
+	{
+		for (int i = 0; i < list.Count; ++i)
+			list[i].LoadItem(player.items[i]);
+	}
+
 	void Update ()
 	{
 		if (Input.GetKeyDown(KeyCode.I))
