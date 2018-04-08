c22154f537afaa91b25f32dd07926f9594fadc87
Arthur MASSON <amasson@e2r7p12.42.fr>
2018-04-08 22:55:02 +0200

    auto attack boost

---
+++ Assets/Scripts/Entity/Spells/SpellEntities/AttackBoostEntity.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackBoostEntity : SpellEntity {
+	
+	float duration;
+
+	void Start () {
+		caster.unit.factorDamages *= 1.1f * skillLevel;
+		duration = 60.0f + 30.0f * skillLevel;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = caster.transform.position;
+		duration -= Time.deltaTime;
+		if (duration <= 0.0f) {
+			caster.unit.factorDamages /= 1.1f * skillLevel;
+			Destroy(this);
+		}
+	}
+}
